02a077656560b9d6580ac7266cf435295cf79c85
triciahoover <tricia.l.hoover@gmail.com>
2017-07-12 11:06:43 -0400

    Finished Addition working

--- addition.rb
+++ addition.rb
@@ -1,3 +1,13 @@
 #This is an addition file
 x = 1 + 1
-print "1 + 1 = #{x}"
+p "1 + 1 = #{x}"
+
+def addition(a,b)
+	p "#{a} + #{b} = #{a + b}"
+end
+
+1.times do
+ addition(1,2)
+ addition(5,6)
+ addition(7,8)
+ end
